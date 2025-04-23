ca62abe95cbf82d6880a1defbf66e39d80076083
Paul Daniel <paudan22@gmail.com>
2025-04-23 12:09:44 +0000

    predictions to numpy

--- eotorch/inference/inference_utils.py
+++ eotorch/inference/inference_utils.py
@@ -1,10 +1,40 @@
 from pathlib import Path
-from typing import Optional, Tuple, Union
+from typing import Any, Optional, Tuple, Union
 
 import numpy as np
 import rasterio as rst
 from affine import Affine
-from rasterio.windows import Window
+from rasterio.windows import Window, from_bounds
+
+
+def prediction_to_numpy(pred: Any) -> np.ndarray:
+    """
+    Convert the prediction to a numpy array.
+
+    Parameters
+    ----------
+        pred: prediction
+
+    Returns
+    ----------
+        numpy array
+    """
+    import torch
+
+    if isinstance(pred, np.ndarray):
+        return pred
+    elif isinstance(pred, torch.Tensor):
+        # check if the tensor is on GPU and / or requires gradients
+        if pred.is_cuda:
+            pred = pred.cpu()
+        if pred.requires_grad:
+            pred = pred.detach()
+        return pred.numpy()
+
+    else:
+        raise ValueError(
+            "Invalid prediction type. Must be numpy array or torch tensor."
+        )
 
 
 def crop_np_to_window(arr: np.ndarray, w_buffered: Window, w_unbuffered: Window):
@@ -42,19 +72,34 @@ def buffered_to_unbuffered(
     if top > 0:  # This is not the first row, so so we want the buffer on top
         window_height -= buffer
         top += buffer
+    elif top < 0:
+        window_height = max(window_height + top, 0)
+        top = 0
+
     if left > 0:  # This is not the first column, so we want the buffer on the left
         window_width -= buffer
         left += buffer
+    elif left < 0:
+        window_width = max(window_width + left, 0)
+        left = 0
     bottom = top + window_height
     right = left + window_width
     if (
         bottom < img_height
     ):  # This is not the last row, so we want the buffer on the bottom
         window_height -= buffer
+    elif bottom > img_height:
+        # we cannot have a window that is larger than the image
+        # so we need to adjust the height
+        window_height = img_height - top
     if (
         right < img_width
     ):  # This is not the last column, so we want the buffer on the right
         window_width -= buffer
+    elif right > img_width:
+        # we cannot have a window that is larger than the image
+        # so we need to adjust the width
+        window_width = img_width - left
 
     return Window(left, top, window_width, window_height)
 
@@ -109,6 +154,82 @@ def buffered_to_unbuffered_new(
     window = Window(left, top, window_width, window_height)
 
     return window, new_transform
+
+
+def eotorch_patch_generator(
+    tif_file_path: str | Path,
+    checkpoint_path: str | Path,
+    batch_size: int = 8,
+):
+    """
+    Similar to the patch_generator function, but uses the exact same way of loading the image data
+    as we do during training with eotorch.
+
+    Yields batches of image data and their corresponding windows.
+    """
+    import torch
+
+    from eotorch.data import SegmentationDataModule
+    from eotorch.data.geodatasets import get_segmentation_dataset
+    from eotorch.data.tasks import SemanticSegmentationTask
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    lightning_module = SemanticSegmentationTask.load_from_checkpoint(
+        checkpoint_path,
+        map_location=device,
+    )
+    lightning_module.eval()
+    data_module_params = torch.load(
+        checkpoint_path, weights_only=False, map_location=device
+    ).get("datamodule_hyper_parameters")
+    if not data_module_params:
+        raise ValueError(
+            "No datamodule_hyper_parameters found in checkpoint. "
+            "Please provide a valid checkpoint with datamodule_hyper_parameters."
+        )
+    lightning_module.datamodule_params = data_module_params
+    dataset_args = data_module_params.get("dataset_args", {})
+
+    pred_ds = get_segmentation_dataset(
+        images_dir=tif_file_path,
+        all_image_bands=dataset_args.get("all_image_bands"),
+        rgb_bands=dataset_args.get("rgb_bands"),
+        crs=dataset_args.get("crs"),
+        res=dataset_args.get("res"),
+        reduce_zero_label=dataset_args.get("reduce_zero_label"),
+        class_mapping=dataset_args.get("class_mapping"),
+        cache_size=0,
+        # cache_size=1,
+        bands_to_return=dataset_args.get("bands_to_return"),
+    )
+    module = SegmentationDataModule(
+        val_dataset=pred_ds,
+        # predict_dataset=pred_ds,
+        patch_size=data_module_params.get("patch_size"),
+        batch_size=batch_size,
+    )
+    module.setup(stage="validate")
+    # module.setup(stage="predict")
+    data_loader = module.val_dataloader()
+    # data_loader = module.predict_dataloader()
+
+    with rst.open(tif_file_path) as src:
+        transform = src.transform
+
+    for batch in data_loader:
+        pixel_windows = [
+            from_bounds(
+                left=bounds[0],
+                bottom=bounds[2],
+                right=bounds[1],
+                top=bounds[3],
+                transform=transform,
+            )
+            for bounds in batch["bounds"]
+        ]
+        batch = module.transfer_batch_to_device(batch, device)
+        yield batch["image"], pixel_windows
+        # yield batch["image"].to(device), pixel_windows
 
 
 def patch_generator(
